3ad074284aff7137332a0887ab9250f9b8ecb162
Oskar <oskarradonkimball@gmail.com>
2016-01-15 09:59:22 -0800

    Starting up the js 🚀

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
-// on document load
-// on submit button event
-// make an array of all the answers
-// for each answer (5 total)
-    // see how many of each equal result1, 2, or 3
+$(document).ready(function (){    // on document load
+  $("form#something").submit(function (event){    // on submit button event
+    // make an array of all the answers
+    // for each answer (5 total)
+        // see how many of each equal result1, 2, or 3
 
-    // if >3 questions === result1 show result1 div
-    // if >3 questions === result2 show result2 div
-    // if >3 questions === result3 show result3 div
-    // else show "Please try again"
-    // if any === blank show "Please answer all questions"
-
-
-// prevent submit event
+        // if >3 questions === result1 show result1 div
+        // if >3 questions === result2 show result2 div
+        // if >3 questions === result3 show result3 div
+        // else show "Please try again"
+        // if any === blank show "Please answer all questions"
+    event.preventDefault();   // prevent submit event
+  });
+});
