3c2e17ff37e2e4e4486aede1994ba919f9fb7ff1
Oskar <oskarradonkimball@gmail.com>
2016-01-15 11:13:37 -0800

    Missed a }
    ⛑

--- js/scripts.js
+++ js/scripts.js
@@ -11,17 +11,16 @@ $(document).ready(function () {                           // on document load
 
     console.log(answers);
     answers.forEach(function(answer) {      // for each answer
-    if () {                                 // if 3 or more answers = paris
-      $("#paris").show();                   // show paris div
-    } else if () {                          // else if 3 or more answers = tokyo
-      $("#tokyo").show();                   // show tokyo div
-    } else if () {                          // else if 3 or more answers = bahamas
-      $("#bahamas").show();                 // show bahamas div
-    } else if () {                          // else if no options:checked
-      $("#answerAll").show();               // show answerAll div
-    } else {                                // else
-      $("#tryAgain").show();                // show tryAgain div
-    }
+      if (3 <= "paris") {                                 // if 3 or more answers = paris
+        $("#paris").show();                   // show paris div
+      } else if (3 <= "tokyo") {                          // else if 3 or more answers = tokyo
+        $("#tokyo").show();                   // show tokyo div
+      } else if (3 <= "bahamas") {                          // else if 3 or more answers = bahamas
+        $("#bahamas").show();                 // show bahamas div
+      } else {                                // else
+        $("#tryAgain").show();                // show tryAgain div
+      }                
+    });
 
     event.preventDefault();   // prevent submit event
   });
