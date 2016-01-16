d5c4d1dc2d1e6b774ae9996c1a070e014421afb2
Oskar <oskarradonkimball@gmail.com>
2016-01-15 16:30:32 -0800

    Answers don't work
    🤕

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $("form#vacation").submit(function (event) {
-    $(".result").hide();         // why is this not working?
+    $(".result").hide();
 
     var locationAnswer = parseInt($("select#location").val());
     var importantAnswer = parseInt($("select#important").val());
@@ -11,12 +11,12 @@ $(document).ready(function () {
     var total = locationAnswer + importantAnswer + foodAnswer + activityAnswer + stayAnswer;
 
     console.log(total);
-      if (total <= 6) {
+      if (total <= 11) {
         $("#paris").show();
-      } else if (total > 6 && total <= 12) {
+      } else if (total > 11 && total <= 14) {
         $("#tokyo").show();
       } else if (total > 13 && total <= 18) {             // this logic isn't working :/
-        $("#bahamas").show();
+        $("#bahamas").show();                             // the answers have overlaps
       } else if (total > 19 && total <= 24) {
         $("#space").show();
       } else {
